14de79f0516b106be013f20ea761dd1b1e108f73
Pavel_Orekhov <Pavel_Orekhov@epam.com>
2019-03-13 17:40:46 +0300

    Intermediate changes (couldn't force push, because I'm on master).

--- src/main/scala/frontend/pascal/PascalErrorCode.scala
+++ src/main/scala/frontend/pascal/PascalErrorCode.scala
@@ -9,7 +9,7 @@ class PascalErrorCode(val errorCode: Int, val message: String) extends Enumerati
     this(0, message)
   }
 
-  val ALREADY_FORWARDED = Value("Already specified in  FORWARD")
+  val ALREADY_FORWARDED = Value("Already specified in FORWARD")
 
 
 }
